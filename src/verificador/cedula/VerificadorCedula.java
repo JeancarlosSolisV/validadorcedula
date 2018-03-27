7744d209e9910f0db6b20d344346622e36bd89d0
JeancarlosSolisV <JeancarlosSolisV@users.noreply.github.com>
2018-03-27 15:34:31 -0500

    verificador cedula
    agregamos  la verificacion del iltimo digito

--- src/verificador/cedula/VerificadorCedula.java
+++ src/verificador/cedula/VerificadorCedula.java
@@ -22,6 +22,8 @@ public class VerificadorCedula {
         // TODO code application logic here
         char[]cI=new char[10];
         int[]cE=new int[10];
+        int[]mUL={2,1,2,1,2,1,2,1,2};
+        int aCUM=0;
         System.out.println("Bienvenido al verificador de cedulas ecuador");
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
@@ -29,14 +31,35 @@ public class VerificadorCedula {
         if(cedula != null){
             if(!cedula.equals("")){
                 if(cedula.length()<=10){
-                    cedula.getChars(0, 9,cI, 0);
+                    cedula.getChars(0, 10,cI, 0);
                     for (int i=0;i<=9;i++){
                         cE[i]=cI[i]-48;
                     }
                     if (cE[2]<6){
-                        
-                        
-                    }else{
+                        for(int i=0;i<=8;i++){
+                            cE[i]=cE[i]*mUL[i];
+                            if (cE[i]>=10){
+                                while (cE[i]>=10){
+                                    cE[i]=cE[i]-9;                                    
+                                }
+                            }                            
+                        }
+                        for(int i=0;i<=8;i++){
+                            aCUM=aCUM+cE[i];
+                        }
+                        if (aCUM%10!=0){
+                            int tEMP=((aCUM/10)+1)*10;
+                            int rES=tEMP-aCUM;
+                            if(rES==cE[9]){
+                                System.out.println("cedula valida");
+                            
+                            }
+                            else{
+                               System.out.println("Cedula invalida");
+                            }
+                        }
+                    }
+                        else{
                         System.out.println("error en la cedula");
                     }
                 }
